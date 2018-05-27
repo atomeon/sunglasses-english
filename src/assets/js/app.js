a3e2d90f5f3aaa3c40628fd37ed470121c84295d
atomeon <atomeon@gmail.com>
2018-05-27 18:16:01 +0600

    feat: mobile menu and animate anchors

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,3 +12,18 @@ import "./modules/sliders";
 
 
 $(document).foundation();
+
+$('.header-nav .menu-x a, .mobile-menu ul a, .header__title a, .hero-logo').click(function (e) {
+	e.preventDefault();
+	let target = $(this).attr('href');
+	if ($('.section-intro__navigation').innerWidth() < 768) {
+		$('html, body').animate({
+			scrollTop: $(target).offset().top - 61
+		}, 800);
+	} else {
+		$('html, body').animate({
+			scrollTop: $(target).offset().top - 80
+		}, 800);
+		$('#offCanvas').foundation('close');
+	}
+});
