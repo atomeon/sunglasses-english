222f4dab3ac9976ef31d70964b96760272a86cf4
Admin <admin@Admins-iMac.local>
2018-06-01 18:57:57 +0600

    fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,12 +11,13 @@ import './lib/foundation-explicit-pieces';
 import "./modules/sliders";
 import * as lazySizes from 'lazysizes';
 import * as from from 'lazysizes/plugins/unveilhooks/ls.unveilhooks.js';
+
 lazySizes.init();
 $(document).foundation();
 
 window.onload = function () {
-
 	document.querySelector('.preloader').remove();
+	console.log('Learn english with us, call now!');
 	$('.header-nav .menu-x a, .mobile-menu ul a, .header__title a, .hero-logo').click(function (e) {
 		e.preventDefault();
 		let target = $(this).attr('href');
