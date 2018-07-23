d008db23594e78120446c689e77ce9408d7e9558
atomeon <atomeon@gmail.com>
2018-07-23 21:07:02 +0600

    feat: add programs section

--- src/assets/js/lib/foundation-explicit-pieces.js
+++ src/assets/js/lib/foundation-explicit-pieces.js
@@ -11,7 +11,7 @@ import { Timer } from 'foundation-sites/js/foundation.util.timer';
 import { Touch } from 'foundation-sites/js/foundation.util.touch';
 import { Triggers } from 'foundation-sites/js/foundation.util.triggers';
 // import { Abide } from 'foundation-sites/js/foundation.abide';
-// import { Accordion } from 'foundation-sites/js/foundation.accordion';
+import { Accordion } from 'foundation-sites/js/foundation.accordion';
 // import { AccordionMenu } from 'foundation-sites/js/foundation.accordionMenu';
 // import { Drilldown } from 'foundation-sites/js/foundation.drilldown';
 // import { Dropdown } from 'foundation-sites/js/foundation.dropdown';
@@ -60,7 +60,7 @@ Triggers.init($, Foundation);
 
 // Foundation.plugin(Abide, 'Abide');
 
-// Foundation.plugin(Accordion, 'Accordion');
+Foundation.plugin(Accordion, 'Accordion');
 
 // Foundation.plugin(AccordionMenu, 'AccordionMenu');
 
